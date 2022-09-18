53955bb9219694c974438c70b46d2287636abe10
Balaram <balaram.venugopal@mdoc.one>
2022-09-18 17:25:56 +0200

    Updated to add External Data

--- data/data.ts
+++ data/data.ts
@@ -3,19 +3,19 @@ import * as Chance from 'chance';
 const chance = new Chance();
 
 let signUpData = {
-    "email": `demoss-${chance.integer}@mailinator.com`,
+    "email": `demoss-${chance.guid()}@mailinator.com`,
     "platform": "web",
     "language": "en",
     "signup_location": "webapp",
     "delayed_confirmation_possible": true,
     "university": null,
     "course_of_studies": null,
-    "password": "IfImtVjIygRf93i5",
+    "password": `${chance.apple_token()}`,
     "tracking_number": 654
 }
 
 let setData = {
-    "name": `APIT-${chance.integer}`,
+    "name": `APIT-${chance.integer()}`,
     "colorId": 0,
     "shared": true,
     "exam_date": new Date(),
