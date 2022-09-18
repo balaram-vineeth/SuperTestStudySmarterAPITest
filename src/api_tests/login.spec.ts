c0424197baa47e4c8639d4cc1c6f014755d2c10e
Balaram <balaram.venugopal@mdoc.one>
2022-09-18 18:42:14 +0200

    Updated the tests

--- src/api_tests/login.spec.ts
+++ src/api_tests/login.spec.ts
@@ -9,7 +9,7 @@ describe('The user is able to signup and create a study set', () => {
     let user_id;
     let token;
     it('The user is able to signup', async()=> {
-        await client().post('https://be.dev.studysmarter-test.de/users/')
+        await client().post('/users/')
         .send(signUpData).expect(201).then((reponse) => {
             expect(reponse.body.id).not.toBe(null);
             expect(reponse.body.token).not.toBe(null);   
@@ -20,11 +20,11 @@ describe('The user is able to signup and create a study set', () => {
 
     it(('The user should be able to create a set'),async () => {
 
-        let res = await client().post(`https://be.dev.studysmarter-test.de/users/${user_id}/course-subjects/`).set('authorization', `Token ${token}`).send(setData).expect(200);
+        let res = await client().post(`/users/${user_id}/course-subjects/`).set('authorization', `Token ${token}`).send(setData).expect(201);
 
         expect(res.body.id).not.toBe(null);
         
-        let study_set = await client().get(`https://be.dev.studysmarter-test.de/users/${user_id}/course-subjects/${res.body.id}/`).set('authorization', `Token ${token}`).send();
+        let study_set = await client().get(`/users/${user_id}/course-subjects/${res.body.id}/`).set('authorization', `Token ${token}`).send();
         checkStatusCode(study_set, 200);
 
         expect(study_set.body.name).toBe(setData.name);
